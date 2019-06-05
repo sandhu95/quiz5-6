10a8640a6df3ad31ad1a404c34aaac05595294a9
Manpreet <Manpreet@DESKTOP-8VTP474>
2019-06-04 17:03:49 -0800

    program containing part 1,2,4

--- Quiz56.java
+++ Quiz56.java
@@ -3,16 +3,23 @@ public class Quiz56 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
+		System.out.println(studying("Shivam"));
+		System.out.println(studying(null));
 		System.out.println(studying("Romil,Jao"));
 		System.out.println(studying("Naveen,Carolini"));
 		
 	}
 	public static String studying(String name)
 	{
-		String x=name.substring(name.indexOf(",")+1);
+		if(name==null)
+			return "Nobody is studying";
+		if(name.indexOf(",")>0)
+		{
+			String x=name.substring(name.indexOf(",")+1);
 		String y=name.substring(0, name.indexOf(","));
 		return y+" and "+x +" are studying";
+		}
+		return name+ " is typing";
 	}
 
 }
